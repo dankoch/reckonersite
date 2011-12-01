847f0689a6601ff4e733f359bef9d6dca2d9c9b8
Dan <dan@reckonlabs.org>
2011-12-01 04:05:22 -0500

    START RELEASE 0.9.12-SNAPSHOT

--- src/reckonersite/settings.py
+++ src/reckonersite/settings.py
@@ -2,11 +2,11 @@
 from mongoengine import connect
 
 #### Version (used for internal tracking purposes) ############################################
-SITE_VERSION="0.9.11"
+SITE_VERSION="0.9.12-SNAPSHOT"
 
 
 #### Environment Version (determines which settings to use, based on the specified environment)
-ENVIRONMENT="prod"
+ENVIRONMENT="local"
 
 
 #### Application Name -- used for identifying permissions and other labeling ##################
